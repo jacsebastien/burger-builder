4abe377915437a6927bd4ce3aae1352dd4a337e1
Sébastien Jacques <sja@bizzdev.net>
2019-12-16 17:24:50 +0100

    Outputting ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import React from 'react';
 import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = () => {
+const Burger = (props) => {
+    // Convert object keys into an array and create number of element equals to each key value
+    const ingredientsArray = Object.keys(props.ingredients)
+        .map(key => {
+            return [...Array(props.ingredients[key])].map((_, i) => {
+                return <BurgerIngredient key={key + i} type={key} />;
+            });
+        });
     return (
         <section className={styles.burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {ingredientsArray}
             <BurgerIngredient type="bread-bottom" />
         </section>
     );
