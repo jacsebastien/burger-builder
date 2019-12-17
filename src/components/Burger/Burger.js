e30823f9364c1d08584a80512097aa40f6c053d7
Sébastien Jacques <sja@bizzdev.net>
2019-12-17 12:05:34 +0100

    Calculating ingredients sum dynamically and show empty message

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
     // Convert object keys into an array and create number of element equals to each key value
-    const ingredientsArray = Object.keys(props.ingredients)
+    let ingredientsArray = Object.keys(props.ingredients)
         .map(key => {
             return [...Array(props.ingredients[key])].map((_, i) => {
                 return <BurgerIngredient key={key + i} type={key} />;
             });
-        });
+        })
+        .reduce((array, element) => array.concat(element), []);
+
+    if(!ingredientsArray.length) {
+        ingredientsArray = <p>Please start adding ingredients.</p>;
+    }
+
     return (
         <section className={styles.burger}>
             <BurgerIngredient type="bread-top" />
