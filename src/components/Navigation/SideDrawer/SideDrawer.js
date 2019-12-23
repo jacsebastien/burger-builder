2bf329db6c18a3f312a07e030a2681c3d54c0ef1
Sébastien Jacques <sja@bizzdev.net>
2019-12-23 15:36:39 +0100

    Creating responsive sidebar

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import styles from './SideDrawer.module.css';
+
+const SideDrawer = (props) => {
+    return (
+        <div className={styles.sideDrawer}>
+            <div className={styles.logo}>
+                <Logo />
+            </div>
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
+};
+
+export default SideDrawer;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,9 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const Toolbar = () => (
     <header className={styles.toolbar}>
         <div>MENU</div>
-        <Logo />
+        <div className={styles.logo}>
+            <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
