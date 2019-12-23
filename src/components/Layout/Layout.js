9f8f8793bf60686697b43b53c79bd81c4a1cb059
Sébastien Jacques <sja@bizzdev.net>
2019-12-23 15:56:50 +0100

    Reusing the backdrop Hiding sade drawer on click

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-    <Fragment>
-        <Toolbar />
-        <SideDrawer />
-        <main className={styles.content}>
-            {props.children}
-        </main>
-    </Fragment>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Toolbar />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={styles.content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        );
+    }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
 import styles from './SideDrawer.module.css';
 
 const SideDrawer = (props) => {
+    const attachedClasses = [styles.sideDrawer, (props.open ? styles.open : styles.close)];
+
     return (
-        <div className={styles.sideDrawer}>
-            <div className={styles.logo}>
-                <Logo />
+        <Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={styles.logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Fragment>
     );
 };
 
