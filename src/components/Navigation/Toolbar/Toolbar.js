3513cf46226084acec677557a75b00d52a687ac0
Sébastien Jacques <sja@bizzdev.net>
2019-12-23 16:09:25 +0100

    Adding sidenav toggler button

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const Toolbar = () => (
+import styles from './Toolbar.module.css';
+
+const Toolbar = (props) => (
     <header className={styles.toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <div className={styles.logo}>
             <Logo />
         </div>
