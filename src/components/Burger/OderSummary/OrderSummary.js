2508150ed81078f011a843d212afa0caf0dd3fe0
Sébastien Jacques <sja@bizzdev.net>
2019-12-23 16:25:00 +0100

    Using shouldComponentUpdate for modal

--- src/components/Burger/OderSummary/OrderSummary.js
+++ src/components/Burger/OderSummary/OrderSummary.js
@@ -1,31 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients)
-        .map(ingKey => {
-            return (
-                <li key={ingKey}>
-                    <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
-                </li>
-            );
-        });
-    return (
-        <Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients :</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p><span style={{fontWeight: '500'}}>Total Price:</span> {props.price.toFixed(2)}€</p>
-            <p>Continue to Checkout ?</p>
-            <Button btnType="danger"
-                clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="success"
-                clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Fragment>
-    );
-};
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderSummary] componentDidUpdate');
+    }
+
+    render() {
+        const ingredientsSummary = Object.keys(this.props.ingredients)
+            .map(ingKey => {
+                return (
+                    <li key={ingKey}>
+                        <span style={{ textTransform: 'capitalize' }}>{ingKey}</span>: {this.props.ingredients[ingKey]}
+                    </li>
+                );
+            });
+
+        return (
+            <Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients :</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p><span style={{ fontWeight: '500' }}>Total Price:</span> {this.props.price.toFixed(2)}€</p>
+                <p>Continue to Checkout ?</p>
+                <Button btnType="danger"
+                    clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="success"
+                    clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Fragment>
+        );
+    }
+}
 
 export default OrderSummary;
