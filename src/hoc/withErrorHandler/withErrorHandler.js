283b28a35b54612753636039740d51d4f3e6be5f
Sébastien Jacques <sja@bizzdev.net>
2019-12-24 11:42:29 +0100

    Handling errors with modal and hoc

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,42 @@
+/* eslint-disable react/display-name */
+import React, { Fragment, Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, Axios) => {
+    // return anonymous class
+    return class extends Component {
+        state = {
+            error: null
+        };
+
+        componentDidMount () {
+            Axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+            });
+
+            Axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error});
+            });
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null});
+        }
+
+        render() {
+            return (
+                <Fragment>
+                    <Modal
+                        show={this.state.error}
+                        modalClosed={this.errorConfirmedHandler}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Fragment>
+            );
+        }
+    };
+};
+
+export default withErrorHandler;
