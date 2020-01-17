61eb489bf642f7c5a601cd7345d8dbaec6e2ad60
Sébastien Jacques <sja@bizzdev.net>
2020-01-17 11:03:07 +0100

    Setting up routing and routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,8 +8,10 @@ import Checkout from './containers/Checkout/Checkout';
 function App() {
     return (
         <Layout>
-            <BurgerBuilder />
-            <Checkout />
+            <Switch>
+                <Route path="/checkout" component={Checkout} />
+                <Route path="/" exact component={BurgerBuilder} />
+            </Switch>
         </Layout>
     );
 }
