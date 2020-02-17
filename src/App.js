77d55d1eba3eb0e8a5206eb0943e48cd926fbda4
Sébastien Jacques <sja@bizzdev.net>
2020-02-17 10:12:41 +0100

    Fixing lazy loaded components with props

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = (props) => {
 
     let routes = (
         <Switch>
-            <Route path="/auth" render={() => <Auth />}/>
+            <Route path="/auth" render={(props) => <Auth {...props} />} />
             <Route path="/" exact component={BurgerBuilder} />
             {/* Redirect to Home page if no route mathes */}
             <Redirect to="/" />
@@ -36,10 +36,10 @@ const App = (props) => {
     if (props.isAuth) {
         routes = (
             <Switch>
-                <Route path="/checkout" render={() => <Checkout />} />
-                <Route path="/orders" render={() => <Orders />} />
+                <Route path="/checkout" render={(props) => <Checkout {...props} />} />
+                <Route path="/orders" render={(props) => <Orders {...props} />} />
                 <Route path="/logout" component={Logout} />
-                <Route path="/auth" render={() => <Auth />} />
+                <Route path="/auth" render={(props) => <Auth {...props} />} />
                 <Route path="/" exact component={BurgerBuilder} />
             </Switch>
         );
@@ -47,7 +47,7 @@ const App = (props) => {
 
     return (
         <Layout>
-            <Suspense fallback={<Spinner/>}>
+            <Suspense fallback={<Spinner />}>
                 {routes}
             </Suspense>
         </Layout >
