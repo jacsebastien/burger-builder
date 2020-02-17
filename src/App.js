b28ba65b56044ff931456f29d365e56747120bc8
Sébastien Jacques <sja@bizzdev.net>
2020-02-17 09:49:06 +0100

    Converting Layout component Managing lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import Spinner from './components/UI/Spinner/Spinner';
 import * as actions from './store/actions';
 
-function App(props) {
+const Checkout = React.lazy(() => {
+    return import('./containers/Checkout/Checkout');
+});
+const Orders = React.lazy(() => {
+    return import('./containers/Orders/Orders');
+});
+const Auth = React.lazy(() => {
+    return import('./containers/Auth/Auth');
+});
+
+// function App(props) {
+const App = (props) => {
     useEffect(() => {
         props.onCheckAuthState();
     }, []);
 
     let routes = (
         <Switch>
-            <Route path="/auth" component={Auth} />
+            <Route path="/auth" render={() => <Auth />}/>
             <Route path="/" exact component={BurgerBuilder} />
             {/* Redirect to Home page if no route mathes */}
             <Redirect to="/" />
@@ -27,10 +36,10 @@ function App(props) {
     if (props.isAuth) {
         routes = (
             <Switch>
-                <Route path="/checkout" component={Checkout} />
-                <Route path="/orders" component={Orders} />
+                <Route path="/checkout" render={() => <Checkout />} />
+                <Route path="/orders" render={() => <Orders />} />
                 <Route path="/logout" component={Logout} />
-                <Route path="/auth" component={Auth} />
+                <Route path="/auth" render={() => <Auth />} />
                 <Route path="/" exact component={BurgerBuilder} />
             </Switch>
         );
@@ -38,10 +47,12 @@ function App(props) {
 
     return (
         <Layout>
-            {routes}
+            <Suspense fallback={<Spinner/>}>
+                {routes}
+            </Suspense>
         </Layout >
     );
-}
+};
 
 const mapStateToProps = state => {
     return {
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,40 +1,34 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import styles from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import { connect } from 'react-redux';
 
-class Layout extends Component {
-    state = {
-        showSideDrawer: false
+const Layout = (props) => {
+    const [showSideDrawer, setShowSideDrawer] = useState(false);
+
+    const sideDrawerClosedHandler = () => {
+        setShowSideDrawer(false);
     };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    }
+    const sideDrawerToggleHandler = () => {
+        setShowSideDrawer(!showSideDrawer);
+    };
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !prevState.showSideDrawer};
-        });
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <Toolbar drawerToggleClicked= {this.sideDrawerToggleHandler} isAuth={this.props.isAuth}/>
-                <SideDrawer
-                    open={this.state.showSideDrawer}
-                    closed={this.sideDrawerClosedHandler}
-                    isAuth={this.props.isAuth}/>
-                <main className={styles.content}>
-                    {this.props.children}
-                </main>
-            </Fragment>
-        );
-    }
-}
+    return (
+        <Fragment>
+            <Toolbar drawerToggleClicked={sideDrawerToggleHandler} isAuth={props.isAuth} />
+            <SideDrawer
+                open={showSideDrawer}
+                closed={sideDrawerClosedHandler}
+                isAuth={props.isAuth} />
+            <main className={styles.content}>
+                {props.children}
+            </main>
+        </Fragment>
+    );
+};
 
 const mapStateToProps = state => {
     return {
